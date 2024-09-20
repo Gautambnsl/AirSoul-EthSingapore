042c84d28e9fe9ebf6eac6c330ee117e8842db91
gautambnsl <bansalgautam005@gmail.com>
2024-09-21 06:32:37 +0800

    adding bots

---
+++ Bot/index.js
@@ -0,0 +1,32 @@
+// index.js
+
+const { FhenixBot } = require('./Fhenix_bot');
+const { AirDAOFunction } = require('./AirDao_bot');
+const { hederaBotFunction } = require('./Hedera_bot');
+const { MorphBotFunction } = require('./Morph_bot');
+const { oasisBotFunction } = require('./Oasis_bot');
+const { RootStockBotFunction } = require('./RootStock_bot');
+
+
+// Initialize Telegram bot and handle errors
+try {
+  FhenixBot();
+  hederaBotFunction();
+   AirDAOFunction();
+   MorphBotFunction();
+   oasisBotFunction();
+   RootStockBotFunction();
+} catch (error) {
+  console.error('Error initializing Telegram bot:', error.message);
+}
+
+// Global error handling
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  // process.exit(1); // Optionally exit the process
+});
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+  // process.exit(1); // Optionally exit the process
+});
